85861c00e9e95ed4492ee11ccd32155bc31b5e41
Andrew Kenneth MacLeay <andrew.macleay@coherehealth.com>
2024-11-06 12:02:10 -0500

    wip

--- grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/ReadPreferenceSpec.groovy
+++ grails-datastore-gorm-mongodb/src/test/groovy/org/grails/datastore/gorm/mongo/ReadPreferenceSpec.groovy
@@ -4,27 +4,34 @@ import grails.gorm.annotation.Entity
 import grails.mongodb.MongoEntity
 import com.mongodb.ReadPreference
 import grails.gorm.tests.GormDatastoreSpec
+import org.junit.platform.commons.logging.LoggerFactory
 
 class ReadPreferenceSpec extends GormDatastoreSpec {
     List getDomainClasses() {
         [PotatoFarm]
     }
 
-  void "Gets read preference from collection correctly"() {
-    when: "Read read concern by default"
-    def rp = PotatoFarm.getCollection().getReadPreference()
+    void "Gets read preference from collection correctly"() {
+        when: "Read read concern by default"
+        def rp = PotatoFarm.getCollection().getReadPreference()
 
-    then:"It is primary"
-    rp == ReadPreference.primary()
+        then: "It is primary"
+        rp == ReadPreference.primary()
 
-    when:"Ask for secondary"
-    def rp2 = PotatoFarm.withReadPreference("secondary") {
-        PotatoFarm.getCollection().getReadPreference()
+        when: "Ask for secondary"
+        def rp2 = PotatoFarm.withReadPreference("secondary") {
+            PotatoFarm.getCollection().getReadPreference()
+        }
+
+        then: "It is secondary"
+        rp2 == ReadPreference.secondary()
+
+        when: "After asking for secondary"
+        def rp3 = PotatoFarm.getCollection().getReadPreference()
+
+        then: "It returns to primary"
+        rp3 == ReadPreference.primary()
     }
-
-    then:"It is secondary"
-    rp2 == ReadPreference.secondary()
-  }
 }
 
 @Entity
